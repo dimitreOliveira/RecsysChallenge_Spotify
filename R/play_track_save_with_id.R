865d9d2561a0e36ad53724f1b6c9fb1f57a5501c
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2018-06-03 18:00:52 -0300

    adding pid to training

--- R/play_track_save_with_id.R
+++ R/play_track_save_with_id.R
@@ -2,6 +2,11 @@ library(dplyr)
 
 playlists <- read.csv2("../data/play_track.csv", header = T, sep = ";", stringsAsFactors = F)
 
+p_challenge <- read.csv2("../data/challenge/play_track_challenge.csv", header = T, sep = ";", stringsAsFactors = F)
+
+# adding challenge to train data
+playlists <- rbind(playlists, p_challenge)
+
 playlists$track_id <- as.numeric(factor(playlists$track_uri, 
                                         levels=unique(playlists$track_uri)))
 
