6474878ccd3aaeaf1e736fcf3ffc487a47512036
tatianass <tatiana.silva@ccc.ufcg.edu.br>
2018-06-12 15:17:07 -0300

    translate submission

---
+++ R/translate_submission_track_uri.R
@@ -0,0 +1,37 @@
+library(dplyr)
+
+# translating track id in int to track_uri
+
+
+track_play <- read.csv2("../data/sample_play_track_id.csv", header = T, sep = ";", stringsAsFactors = F)
+submissions <- read.csv2("../submissions/submission1.csv", header = T, sep = ",", stringsAsFactors = F)
+test <- submissions
+
+info <- track_play[, c(2, 4)]
+new <- submissions %>% select(pid)
+
+name <- "trackuri_"
+for(i in colnames(submissions)){
+  if(i != "pid"){
+    df <- submissions[,c("pid", i)]
+    
+    names <- c("pid", "track_id")
+    colnames(df) <- names
+    
+    df_join <- df %>%
+      left_join(info, by = c("track_id")) %>%
+      distinct() %>%
+      select(-pid)
+    
+    
+    names <- c("track_id", i)
+    colnames(df_join) <- names
+    
+    df_join <- df_join %>% select(-track_id)
+    
+    new <- cbind(new, df_join)
+    
+  }
+}
+
+write.table(new, file = "../submissions/submission.csv",row.names=FALSE, na="",col.names=TRUE, sep=",")
